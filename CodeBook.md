63850a9bfe717df6b44548ba95ed66206abb31e5
garry <garryferg@gmail.com>
2015-12-27 21:11:48 +0000

    Initial commit of working run_analysis.R and unfinished documentation

---
+++ CodeBook.md
@@ -0,0 +1,40 @@
+
+# Code Book
+
+## Description of variables
+
+#### Body.Acceleration.Jerk.Mean.[X,Y,Z,Magnitude]
+
+#### Body.Acceleration.Jerk.StdDev.[X,Y,Z,Magnitude]
+
+#### Body.Acceleration.Mean.[X,Y,Z,Magnitude]
+
+#### Body.Acceleration.StdDev.[X,Y,Z,Magnitude]
+
+#### Body.Gyroscope.Jerk.Mean.[X,Y,Z,Magnitude]
+
+#### Body.Gyroscope.Jerk.StdDev.[X,Y,Z,Magnitude]
+
+#### Body.Gyroscope.Mean.[X,Y,Z,Magnitude]
+
+#### Body.Gyroscope.StdDev.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Acceleration.Jerk.Mean.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Acceleration.Jerk.StdDev.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Acceleration.Mean.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Acceleration.StdDev.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Gyroscope.Jerk.Mean.Magnitude
+
+#### FFT.Body.Gyroscope.Jerk.StdDev.Magnitude
+
+#### FFT.Body.Gyroscope.Mean.[X,Y,Z,Magnitude]
+
+#### FFT.Body.Gyroscope.StdDev.[X,Y,Z,Magnitude]
+
+#### Gravity.Acceleration.Mean.[X,Y,Z,Magnitude]
+
+#### Gravity.Acceleration.StdDev.[X,Y,Z,Magnitude]
---
+++ run_analysis.R
@@ -0,0 +1,84 @@
+library(dplyr)
+
+# Unzip data
+# unzip('getdata_projectfiles_UCI HAR Dataset.zip', exdir='unzipped')
+
+# Read train data
+subject_train <- read.table('unzipped/UCI HAR Dataset/train/subject_train.txt', header=FALSE)
+X_train <- read.table('unzipped/UCI HAR Dataset/train/X_train.txt', strip.white=TRUE, header=FALSE)
+y_train <- read.table('unzipped/UCI HAR Dataset/train/y_train.txt', header=FALSE)
+
+# Read test data
+subject_test <- read.table('unzipped/UCI HAR Dataset/test/subject_test.txt', header=FALSE)
+X_test <- read.table('unzipped/UCI HAR Dataset/test/X_test.txt', strip.white=TRUE, header=FALSE)
+y_test <- read.table('unzipped/UCI HAR Dataset/test/y_test.txt', header=FALSE)
+
+# Combine subject, y and all of the X features into data frames
+train <- data.frame(subject_train, y_train, X_train)
+test <- data.frame(subject_test, y_test, X_test)
+
+# Read in names from the features from features.txt. (make.names replaces illegal characters with .)
+featureNames <- make.names(as.character(read.table('unzipped/UCI HAR Dataset/features.txt', header=FALSE)[,2]))
+# Prepend names for first two columns (subject_* and y_*)
+featureNames <- c('subject', 'activity', featureNames)
+# Set names for train and test data frames
+names(train) <- featureNames
+names(test) <- featureNames
+
+# Now join into one large data frame containing test and training data
+all <- rbind(train, test)
+
+# We want to keep any variables whose names originally contained "-mean()" or "-std()"
+# make.names above replaced some characters with . so we search for ".mean.." and ".std.."
+# Find column indices for those variables:
+meanIndicies <- grep(".mean..", featureNames, fixed = TRUE)
+stdDeviationIndicies <- grep(".std..", featureNames, fixed = TRUE)
+
+# Union the column indices together, and add 1 and 2 for the first two columns (subject and y)
+columnsToKeep <- union(meanIndicies, stdDeviationIndicies)
+columnsToKeep <- union(c(1,2), columnsToKeep) #keep subject and activity
+
+# Now filter 'all' down to just the columns we are interested in
+all <- all[,columnsToKeep]
+
+# Clean up the names slightly by stripping multiple "." characters and removing them from the end of names
+names(all) <- sub("...", ".", names(all), fixed = TRUE)
+names(all) <- sub("\\.$", "", names(all))
+names(all) <- sub("\\.$", "", names(all))
+
+# tBodyAcc"             "tGravityAcc"         
+#[6] "tBodyAccJerk"         "tBodyGyro"            "tBodyGyroJerk"        "tBodyAccMag"          "tGravityAccMag"      
+#[11] "tBodyAccJerkMag"      "tBodyGyroMag"         "tBodyGyroJerkMag"     "fBodyAcc"             "fBodyAccJerk"        
+#[16] "fBodyGyro"            "fBodyAccMag"          "fBodyBodyAccJerkMag"  "fBodyBodyGyroMag"     "fBodyBodyGyroJerkMag"
+
+names(all) <- sub("BodyBody", "Body", names(all), fixed = TRUE) #assume BodyBody is a mistake in features.txt
+names(all) <- sub("tBody", "Body.", names(all), fixed = TRUE)
+names(all) <- sub("fBody", "FFT.Body.", names(all), fixed = TRUE)
+names(all) <- sub("tGravity", "Gravity.", names(all), fixed = TRUE)
+names(all) <- sub("Acc", "Acceleration.", names(all), fixed = TRUE)
+names(all) <- sub("Gyro", "Gyroscope.", names(all), fixed = TRUE)
+names(all) <- sub("Jerk", "Jerk.", names(all), fixed = TRUE)
+names(all) <- sub("Mag", "Magnitude.", names(all), fixed = TRUE)
+names(all) <- sub(".mean", ".Mean", names(all), fixed = TRUE)
+names(all) <- sub(".std", ".StdDev", names(all), fixed = TRUE)
+names(all) <- sub("..", ".", names(all), fixed = TRUE)
+names(all) <- sub("Magnitude.Mean", "Mean.Magnitude", names(all), fixed = TRUE)
+names(all) <- sub("Magnitude.StdDev", "StdDev.Magnitude", names(all), fixed = TRUE)
+
+
+
+
+
+
+# Our activity variable currently contains integers. We want to replace them with activity names.
+# Read from activity_labels.txt - there are two columns, and integer key and an activity label
+activityLabels <- read.table("unzipped/UCI HAR Dataset/activity_labels.txt", header=FALSE, col.names=c("activity","activityLabel"))
+# 'Inner join' all and activityLabels to add an activityLabel column
+all <- merge(all, activityLabels, by="activity")
+
+# Now we create the final dataset, calculating the mean across groups,
+# using group_by and summarise_each from dplyr:
+grouped <- group_by(all, subject, activityLabel)
+final <- summarise_each(grouped, funs(mean))
+# And save the final result
+write.table(final, 'final.txt', row.name=FALSE)
